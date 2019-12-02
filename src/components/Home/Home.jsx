ac3a191b0fbf989e18102ddc5ec677b8f329d003
HUYNH MINH NGHIA <nghia.t175711@vanlanguni.vn>
2019-12-02 14:29:32 +0700

    nghixca

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+   render(){
+
+    return (
+        <div className="App">
+            
+
+           
+        </div>
+      );
+   }
+}
+
+
+export default Home;
